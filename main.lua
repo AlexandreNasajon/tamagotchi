e143268186dc916fef40faf061daea952d72547b
nasajon-chan <alexandrenasajon66@hotmail.com>
2020-08-11 19:27:03 -0300

    added boxes

--- main.lua
+++ main.lua
@@ -1,21 +1,24 @@
 --[[
     Tamagotchi by nasa
 
-    LISTA DE ITENS
-    DONE
-    - desenhar bicho
-    - desenhar stats do bicho
-    - feed / play / train
-    - contador que aumenta hunger e boredom
-    - salvar o jogo
-    - new game / continue
     TODO
-    - fazenda com vários bichos
+    
+    FARM
+    - desenhar bichos na fazenda
+    
+    ADVENTURE
     - mandar bicho em aventura
     - encontrar inimigos
     - lutar contra inimigo
     - recrutar inimigo
 ]]
+
+local Farm = require('Farm')
+local Items = require('Items')
+local Equip = require('Equip')
+local Monsters = require('Monsters')
+local Adventure = require('Adventure')
+
 
 -- checa se o player clicou dentro do objeto
 function inBox( x , y , obj )
@@ -23,6 +26,137 @@ function inBox( x , y , obj )
         return true
     end
 end
+
+
+function love.load()
+    font = love.graphics.newFont(24)
+    if not time then time = 0 end
+    animationCounter = 0
+    hungerCounter = 0
+    boredomCounter = 0
+    screenSize = love.window.setMode( 9*40 , 16*40 )
+    backgroundColor = {189/255 , 195/255 , 199/255} -- lilás bem claro
+    love.graphics.setBackgroundColor( backgroundColor )
+    love.graphics.setColor( 0 , 0 , 0 )
+    titleBox = {box = {x = 9*12 , y = 16*2}}
+    optionBox_1 = {box = {x = 9*12 , y = 16*20 , w = 140 , h = 40}}
+    optionBox_2 = {box = {x = 9*12 , y = 16*24 , w = 140 , h = 40}}
+    optionBox_3 = {box = {x = 9*12 , y = 16*28 , w = 140 , h = 40}}
+    optionBox_4 = {box = {x = 9*12 , y = 16*32 , w = 140 , h = 40}}
+    optionBox_5 = {box = {x = 9*12 , y = 16*36 , w = 140 , h = 40}}
+
+    player = {
+        coins = 10,
+        gems = 1,
+        rank = 'Copper'
+    }
+    pet1 = {
+        name = 'Jer',
+        strength = 1,
+        stamina = 5,
+        carisma = 0,
+        img = love.graphics.newImage( "images/pet1.png" ),
+        miniImg = love.graphics.newImage( 'images/pet1mini.png')
+    }
+    pet2 = {
+        name = 'Bel',
+        strength = 1,
+        stamina = 5,
+        carisma = 0,
+        img = love.graphics.newImage( "images/pet2.png" ),
+        miniImg = love.graphics.newImage( 'images/pet2mini.png')
+    }
+    mainPetBox = {pet = pet1 , box = {x = 9*12 , y = 16*4 }}
+    petBox_1 = {pet = pet2 , box = {x = 9*2 , y = 16*8 , w = 50 , h = 50}}
+    petBox_2 = {box = {x = 9*2 , y = 16*12 , w = 50 , h = 50}}
+    petBox_3 = {box = {x = 9*2 , y = 16*16 , w = 50 , h = 50}}
+
+    menu = true
+end
+
+function love.update(dt)
+    saveGame()
+    time = time + 1/60
+end
+
+function love.draw()
+    if menu then
+        love.graphics.draw(love.graphics.newText( font , 'MAIN MENU' ) , titleBox.box.x , titleBox.box.y )
+        love.graphics.draw(love.graphics.newText( font , 'CONTINUE' ) , optionBox_1.box.x , optionBox_1.box.y)
+        love.graphics.draw(love.graphics.newText( font , 'NEW GAME' ), optionBox_2.box.x , optionBox_2.box.y)
+    elseif farm then
+        love.graphics.draw(love.graphics.newText( font , '     FARM' ) , titleBox.box.x , titleBox.box.y  )
+        love.graphics.print(math.floor(time))
+        love.graphics.print('Creature: '..mainPetBox.pet.name , 0 , 16)
+        love.graphics.print('Strength: '..mainPetBox.pet.strength , 0 , 16*2)
+        love.graphics.print('Stamina: '..mainPetBox.pet.stamina , 0 , 16*3)
+        love.graphics.print('Carisma: '..mainPetBox.pet.carisma , 0 , 16*4)
+        love.graphics.print('Coins: '..player.coins , 9*12 , 0)
+        love.graphics.print('Gems: '..player.gems , 9*20 , 0)
+        love.graphics.print('Rank: '..player.rank , 9*28 , 0)
+        love.graphics.draw( mainPetBox.pet.img , mainPetBox.box.x , mainPetBox.box.y )
+        if petBox_1.pet then love.graphics.draw( petBox_1.pet.miniImg , petBox_1.box.x , petBox_1.box.y ) end
+        if petBox_2.pet then love.graphics.draw( petBox_2.pet.miniImg , petBox_2.box.x , petBox_2.box.y ) end
+        if petBox_3.pet then love.graphics.draw( petBox_3.pet.miniImg , petBox_3.box.x , petBox_3.box.y ) end
+        love.graphics.draw(love.graphics.newText( font , 'ADVENTURE' ) , optionBox_4.box.x , optionBox_4.box.y )
+    elseif adventure then
+        love.graphics.draw(love.graphics.newText( font , 'ADVENTURE' ) , titleBox.box.x , titleBox.box.y  )
+        love.graphics.draw(love.graphics.newText( font , 'FOREST') , optionBox_1.box.x , optionBox_1.box.y )
+    end
+end
+
+function atMenu( x , y , button , istouch )
+    if button == 1 then
+        if inBox( x , y , optionBox_1.box) then -- continue
+            loadGame()
+            menu = false
+            farm = true
+        elseif inBox( x , y , optionBox_2.box) then -- new game
+            time = 0
+            menu = false
+            farm = true
+        end
+    end
+end
+
+function atFarm( x , y , button , istouch )
+    if button == 1 then
+        if inBox(x,y,optionBox_4.box) then
+            farm = false
+            adventure = true
+        elseif inBox(x,y,petBox_1.box) then
+            local tmp = petBox_1.pet
+            petBox_1.pet = mainPetBox.pet
+            mainPetBox.pet = tmp
+        end
+    end
+end
+
+--TODO
+function atAdventure( x , y , button , istouch )
+    if button == 1 then
+        if inBox(x,y,optionBox_1.box) then
+            adventure = false
+            farm = true
+        end
+    end
+end
+
+function love.mousepressed( x , y , button , istouch )
+    if menu then
+        atMenu( x , y , button , istouch )
+    elseif farm then
+        atFarm( x , y , button , istouch )
+    elseif adventure then
+        atAdventure( x , y , button , istouch )
+    end
+end
+
+
+-------------------------------- SAVE/LOAD GAME ----------------------------------------
+
+
+
 
 -- peguei na net
 -- recebe o número índice da linha do arquivo e o arquivo
@@ -38,25 +172,27 @@ end
 
 -- pega as informações no arquivo e coloca nas variáveis correspondentes
 function loadGame()
-    pet.name = fileLine(1,'file.txt')
-    pet.strength = tonumber(fileLine(2,'file.txt'))
-    pet.stamina = tonumber(fileLine(3,'file.txt'))
-    pet.friendship = tonumber(fileLine(4,'file.txt'))
-    pet.hunger = tonumber(fileLine(5,'file.txt'))
-    pet.boredom = tonumber(fileLine(6,'file.txt'))
-    time = tonumber(fileLine(7,'file.txt')) 
+    pet1.name = fileLine(1,'file.txt')
+    pet1.strength = tonumber(fileLine(2,'file.txt'))
+    pet1.stamina = tonumber(fileLine(3,'file.txt'))
+    pet1.carisma = tonumber(fileLine(4,'file.txt'))
+    time = tonumber(fileLine(5,'file.txt'))
+    player.coins = tonumber(fileLine(6,'file.txt'))
+    player.gems = tonumber(fileLine(7,'file.txt')) 
+    player.rank = fileLine(8,'file.txt')
 end
 
 -- substitui cada linha do arquivo pelo que estiver nas variáveis do pet
 function saveGame()
     local file = io.open('file.txt','w')
-    file:write(pet.name, "\n")
-    file:write(pet.strength, "\n")
-    file:write(pet.stamina, "\n")
-    file:write(pet.friendship, "\n")
-    file:write(pet.hunger, "\n")
-    file:write(pet.boredom, "\n")
+    file:write(pet1.name, "\n")
+    file:write(pet1.strength, "\n")
+    file:write(pet1.stamina, "\n")
+    file:write(pet1.carisma, "\n")
     file:write(time,"\n")
+    file:write(player.coins, "\n")
+    file:write(player.gems, "\n")
+    file:write(player.rank, "\n")
     file:close()
 end
 
@@ -74,167 +210,4 @@ function love.quit()
 
     ]]
     print("Thanks for playing! Come back soon!")
-  end
-
-function love.load()
-    font = love.graphics.newFont(24)
-    if not time then time = 0 end
-    animationCounter = 0
-    hungerCounter = 0
-    boredomCounter = 0
-    --w, h = love.graphics.getDimensions()
-    screenSize = love.window.setMode( 640, 640 )
-    backgroundColor = {189/255 , 195/255 , 199/255} -- lilás bem claro acinzentado
-    love.graphics.setBackgroundColor( backgroundColor )
-    love.graphics.setColor( 0 , 0 , 0 )
-    continue = {
-        text = love.graphics.newText( font , 'CONTINUE' ),
-        box = {x = 200 , y = 100 , w = 100 , h = 40}
-    }
-    newGame = {
-        text = love.graphics.newText( font , 'NEW GAME' ),
-        box = {x = 200 , y = 150 , w = 100 , h = 40}
-    }
-    pet = {}
-    feed = {
-        text = love.graphics.newText( font , 'FEED' ),
-        box = {x = 200 , y = 400 , w = 100 , h = 40},
-        anim = love.graphics.newText( font , 'FEEDING' )
-    }
-    play = {
-        text = love.graphics.newText( font , 'PLAY' ),
-        box = {x = 200 , y = 440 , w = 100 , h = 40},
-        anim = love.graphics.newText( font , 'PLAYING' )
-    }
-    train = {
-        text = love.graphics.newText( font , 'TRAIN' ),
-        box = {x = 200 , y = 480 , w = 100 , h = 40},
-        anim = love.graphics.newText( font , 'TRAINING' )
-    }
-    adventure = {
-        text = love.graphics.newText( font , 'ADVENTURE' ),
-        box = {x = 200 , y = 520 , w = 100 , h = 40}
-    }
-    animation = love.graphics.newText( font , '' )
-    pet_2 = love.graphics.newImage( "images/Pet_2.png" )
-    loadGame()
-    menu = true
-end
-
-function love.update(dt)
-    saveGame()
-    time = time + 1/60
-    hungerCounter = hungerCounter + dt
-    if hungerCounter > 5 then
-        pet.hunger = pet.hunger + 1
-        hungerCounter = 0
-    end
-    boredomCounter = boredomCounter + dt
-    if boredomCounter > 10 then
-        pet.boredom = pet.boredom + 1
-        boredomCounter = 0
-    end
-    animationCounter = animationCounter + dt
-    if animationCounter >= 4 then
-        if not (animation == love.graphics.newText( font , '' )) then
-            animation = love.graphics.newText( font , '' )
-            animationCounter = 0
-        end
-    end
-end
-
-function love.draw()
-    if menu then
-        mainMenu = love.graphics.newText( font , 'MAIN MENU' )
-        love.graphics.draw(mainMenu , 200 , 50)
-        love.graphics.draw(continue.text , continue.box.x , continue.box.y)
-        love.graphics.draw(newGame.text , newGame.box.x , newGame.box.y)
-    elseif farm then
-        love.graphics.print(math.floor(time))
-        love.graphics.print('Name: '..pet.name , 0 , 20)
-        love.graphics.print('Strength: '..pet.strength , 0 , 40)
-        love.graphics.print('Stamina: '..pet.stamina , 0 , 60)
-        love.graphics.print('Friendship: '..pet.friendship , 0 , 80)
-        love.graphics.print('Hunger: '..pet.hunger , 0 , 100)
-        love.graphics.print('Boredom: '..pet.boredom , 0 , 120)
-        love.graphics.draw(pet_2 , 200 , 200 )
-        love.graphics.draw(play.text , play.box.x , play.box.y )
-        love.graphics.draw(feed.text , feed.box.x , feed.box.y )
-        love.graphics.draw(train.text , train.box.x , train.box.y )
-        love.graphics.draw(adventure.text , adventure.box.x , adventure.box.y )
-        love.graphics.draw(animation , 200 , 100 )
-    elseif inAdventure then
-        -- TODO
-        adv = love.graphics.newText( font , 'ADVENTURE' )
-        love.graphics.draw(adv , 200 , 50)
-        
-    end
-end
-
-function atMenu( x , y , button , istouch )
-    if button == 1 then
-        if inBox( x , y , continue.box) then
-            menu = false
-            farm = true
-        elseif inBox( x , y , newGame.box) then
-            pet = {
-                name = '',
-                strength = 1,
-                stamina = 5,
-                friendship = 0,
-                hunger = 5,
-                boredom = 5
-            }
-            time = 0
-            menu = false
-            farm = true
-        end
-    end
-end
-
-function atFarm( x , y , button , istouch )
-    if button == 1 then
-        if inBox(x,y,play.box) then -- PLAY
-            animation = play.anim
-            if pet.boredom > 0 then
-                pet.boredom = pet.boredom - 1
-            end
-        elseif inBox(x,y,feed.box) then -- FEED
-            animation = feed.anim
-            if pet.hunger > 0 then
-                pet.hunger = pet.hunger - 1
-            end
-        elseif inBox(x,y,train.box) then -- TRAIN
-            animation = train.anim
-            if pet.strength < 10 then
-                pet.strength = pet.strength + 1
-            end
-        elseif inBox(x,y,adventure.box) then
-            farm = false
-            inAdventure = true
-        end
-    end
-end
-
-function atAdventure( x , y , button , istouch )
-    --TODO
-    if button == 1 then
-
-    end
-end
-
-function love.mousepressed( x , y , button , istouch )
-
-    if menu then
-        atMenu( x , y , button , istouch )
-    end
-
-    if farm then
-        atFarm( x , y , button , istouch )
-    end
-
-    if adventure then
-        atAdventure( x , y , button , istouch )
-    end
-
 end
