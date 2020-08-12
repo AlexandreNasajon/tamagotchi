78508fb1b118953e86a31efd8586d9df421f9bb3
nasajon-chan <alexandrenasajon66@hotmail.com>
2020-08-12 04:00:32 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,7 +4,6 @@
     TODO
     
     FARM
-    - desenhar bichos na fazenda
     
     ADVENTURE
     - mandar bicho em aventura
@@ -39,7 +38,7 @@ function love.load()
     backgroundColor = {189/255 , 195/255 , 199/255} -- lilás bem claro
     love.graphics.setBackgroundColor( backgroundColor )
     love.graphics.setColor( 0 , 0 , 0 )
-    titleBox = {box = {x = 9*12 , y = 16*2}}
+    titleBox = {box = {x = 9*12 , y = 16}}
     optionBox = {}
     optionBox[1] = {box = {x = 9*12 , y = 16*20 , w = 140 , h = 40}}
     optionBox[2] = {box = {x = 9*12 , y = 16*24 , w = 140 , h = 40}}
@@ -76,12 +75,39 @@ function love.load()
         img = love.graphics.newImage( "images/pet3.png" ),
         miniImg = love.graphics.newImage( 'images/pet3mini.png')
     }
+
     mainPetBox = {pet = pet1 , box = {x = 9*12 , y = 16*4 }}
-    petBox = {
+
+    farmBox = {
     {pet = pet2 , box = {x = 9*2 , y = 16*8 , w = 50 , h = 50}},
     {pet = pet3 , box = {x = 9*2 , y = 16*13 , w = 50 , h = 50}},
     {box = {x = 9*2 , y = 16*17 , w = 50 , h = 50}}
     }
+
+    selectionBox = {
+        {box = {x = 9*4 , y = 16*8, w = 50 , h = 50}},
+        {box = {x = 9*10 , y = 16*8, w = 50 , h = 50}},
+        {box = {x = 9*18 , y = 16*8, w = 50 , h = 50}},
+        {box = {x = 9*24 , y = 16*8, w = 50 , h = 50}},
+        {box = {x = 9*30 , y = 16*8, w = 50 , h = 50}}
+    }
+
+    teamBox = {
+        {box = {x = 9*4 , y = 16*32, w = 50 , h = 50}},
+        {box = {x = 9*10 , y = 16*32, w = 50 , h = 50}},
+        {box = {x = 9*18 , y = 16*32, w = 50 , h = 50}}
+    }
+
+    explorerBox = {
+        {box = {x = 9*4 , y = 16*32, w = 50 , h = 50}},
+        {box = {x = 9*10 , y = 16*32, w = 50 , h = 50}},
+        {box = {x = 9*18 , y = 16*32, w = 50 , h = 50}}
+    }
+
+    enemyBox = {
+        {box = {x = 9*22 , y = 16*28, w = 50 , h = 50}},
+    }
+
     atMenu = true
 
 
@@ -94,10 +120,10 @@ function drawMainPetBox(x,y)
 end
 
 function drawPetBox(x,y)
-    for i = 1 , #petBox do
-        if petBox[i].pet then 
-            love.graphics.draw( petBox[i].pet.miniImg , petBox[i].box.x + x , petBox[i].box.y + y)
-            love.graphics.draw( love.graphics.newText(miniFont,petBox[i].pet.name) , petBox[i].box.x + 9*2 + x , petBox[i].box.y - 16 + y )
+    for i = 1 , #farmBox do
+        if farmBox[i].pet then 
+            love.graphics.draw( farmBox[i].pet.miniImg , farmBox[i].box.x + x , farmBox[i].box.y + y)
+            love.graphics.draw( love.graphics.newText(miniFont,farmBox[i].pet.name) , farmBox[i].box.x + 9*2 + x , farmBox[i].box.y - 16 + y )
         end
     end
 end
@@ -105,6 +131,9 @@ end
 function love.update(dt)
     saveGame()
     time = time + 1/60
+    if atForest then
+        
+    end
 end
 
 function love.draw()
@@ -113,7 +142,7 @@ function love.draw()
         love.graphics.draw(love.graphics.newText( font , 'MAIN MENU' ) , titleBox.box.x , titleBox.box.y )
         love.graphics.draw(love.graphics.newText( font , '> CONTINUE' ) , optionBox[1].box.x , optionBox[1].box.y)
         love.graphics.draw(love.graphics.newText( font , '> NEW GAME' ), optionBox[2].box.x , optionBox[2].box.y)
-        
+
     elseif atFarm then
 
         love.graphics.draw(love.graphics.newText( font , '     FARM' ) , titleBox.box.x , titleBox.box.y  )
@@ -137,10 +166,32 @@ function love.draw()
     elseif atTeamSelectionScreen then
 
         love.graphics.draw(love.graphics.newText( font , 'SELECT YOUR TEAM' ) , titleBox.box.x - 9*4 , titleBox.box.y  )
-        drawMainPetBox(9*14,16*9)
-        drawPetBox(9*12,16*6)
+        for i = 1 , #selectionBox do
+            if selectionBox[i].pet then
+                love.graphics.draw( selectionBox[i].pet.miniImg , selectionBox[i].box.x , selectionBox[i].box.y )
+            end
+        end
         love.graphics.draw(love.graphics.newText(font , '> CONFIRM TEAM') , optionBox[3].box.x , optionBox[3].box.y)
+        for i = 1 , #teamBox do
+            if teamBox[i].pet then
+                love.graphics.draw( teamBox[i].pet.miniImg , teamBox[i].box.x , teamBox[i].box.y )
+            end
+        end
 
+    elseif atForest then
+
+        love.graphics.draw(love.graphics.newText( font , 'FOREST' ) , titleBox.box.x , titleBox.box.y  )
+        love.graphics.draw( Adventure.Dungeons.Forest.img , -9*4 , 16*4 )
+        for i = 1 , #explorerBox do
+            if explorerBox[i].pet then
+                love.graphics.draw(love.graphics.newText( miniFont , explorerBox[i].pet.name ) , explorerBox[i].box.x -9*2, explorerBox[i].box.y - 16)
+                love.graphics.draw(explorerBox[i].pet.miniImg , explorerBox[i].box.x -9*4 , explorerBox[i].box.y )
+            end
+        end
+        if enemyBox.pet then
+            love.graphics.draw(love.graphics.newText( miniFont , enemyBox.pet.name ) , enemyBox[1].box.x + 9*8 , enemyBox[1].box.y )
+            love.graphics.draw(enemyBox.pet.img , enemyBox[1].box.x , enemyBox[1].box.y )
+        end
     end
 end
 
@@ -163,10 +214,10 @@ function FarmScreen( x , y , button , istouch )
             atFarm = false
             atDungeonSelectionScreen = true
         end
-        for i = 1 , #petBox do
-            if inBox(x,y,petBox[i].box) then
-                local tmp = petBox[i].pet
-                petBox[i].pet = mainPetBox.pet
+        for i = 1 , #farmBox do
+            if inBox(x,y,farmBox[i].box) then
+                local tmp = farmBox[i].pet
+                farmBox[i].pet = mainPetBox.pet
                 mainPetBox.pet = tmp
                 tmp = nil
             end
@@ -174,22 +225,50 @@ function FarmScreen( x , y , button , istouch )
     end
 end
 
-function teamSelectionScreen( x , y , button , istouch )
-    if button == 1 then
-        if inBox(x,y,optionBox[3].box) then
-            atTeamSelectionScreen = false
-            atFarm = true
-        end
-    end
-end
-
 function dungeonSelectionScreen( x , y , button , istouch )
     if button == 1 then
         if inBox(x,y,optionBox[1].box) then
+            selectionBox[1].pet = mainPetBox.pet
+            if farmBox[1].pet then selectionBox[2].pet = farmBox[1].pet end
+            if farmBox[2].pet then selectionBox[3].pet = farmBox[2].pet end 
+            if farmBox[3].pet then selectionBox[4].pet = farmBox[3].pet end
             atDungeonSelectionScreen = false
             atTeamSelectionScreen = true
         end
     end
+end
+
+function teamSelectionScreen( x , y , button , istouch )
+    if button == 1 then
+        if inBox(x,y,optionBox[3].box) then
+            if teamBox[1].pet then -- check if there is any pet selected
+                enemyBox.pet = Adventure.Dungeons.Forest.enemies[1]
+                atTeamSelectionScreen = false
+                atForest = true
+            end
+        end
+        for j = 1 , #selectionBox do
+            if inBox(x,y,selectionBox[j].box) then
+                if not teamBox[1].pet then
+                    teamBox[1].pet = selectionBox[j].pet
+                    explorerBox[1].pet = teamBox[1].pet
+                    selectionBox[j].pet = nil
+                elseif not teamBox[2].pet then
+                    teamBox[2].pet = selectionBox[j].pet
+                    explorerBox[2].pet = teamBox[2].pet
+                    selectionBox[j].pet = nil
+                elseif not teamBox[3].pet then
+                    teamBox[3].pet = selectionBox[j].pet
+                    explorerBox[3].pet = teamBox[3].pet
+                    selectionBox[j].pet = nil
+                end
+            end
+        end
+    end
+end
+
+function ForestScreen()
+    enemyBox.pet = Adventure.Dungeons.Forest.enemies[1]
 end
 
 function love.mousepressed( x , y , button , istouch )
@@ -201,14 +280,13 @@ function love.mousepressed( x , y , button , istouch )
         dungeonSelectionScreen( x , y , button , istouch )
     elseif atTeamSelectionScreen then
         teamSelectionScreen( x , y , button , istouch )
+    elseif atForest then
+        ForestScreen()
     end
 end
 
 
 -------------------------------- SAVE/LOAD GAME ----------------------------------------
-
-
-
 
 -- peguei na net
 -- recebe o número índice da linha do arquivo e o arquivo
